b97851ac1a7ae2f0c4ee76c4dba64f088078799c
kyle <kponeill124@gmail.com>
2025-01-08 14:56:59 -0500

    updates Chip8.hpp with functions from Chip8.cpp as well as other functionality

--- Chip8.hpp
+++ Chip8.hpp
@@ -8,47 +8,155 @@
 #include <chrono>
 #include <random>
 
-class Chip8 //class for the chip8 components
-// created a class with the following components, we can now make variables with these attributes
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+
+
+class Chip8
 {
-public: // can be called from any scope in the program
+public:
+	Chip8();
+	void LoadROM(char const* filename);
+	void Cycle();
 
-    Chip8(); // constructor delaration, automatically runs when we create an object to setup, initializes the contents of the object
+	uint8_t keypad[KEY_COUNT]{};
+	uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT]{};
 
-    uint8_t registers[16]{};
-    uint8_t memory[4096]{};
-    uint16_t index{};
-    uint16_t pc{};
-    uint16_t stack[16];
-    uint8_t sp{};
-    uint8_t delayTimer{};
-    uint8_t soundTimer{};
-    uint8_t keypad[16]{};
-    uint32_t video[64 * 32]{};
-    uint16_t opcode;
+private:
+	void Table0();
+	void Table8();
+	void TableE();
+	void TableF();
 
-private: // cannot be called directly outside of the class, cannot be manipulated outside the class
+	// Do nothing
+	void OP_NULL();
 
-    // CLS
-    void OP_00E0();
-    
-    // RET
-    void OP_00EE();
+	// CLS
+	void OP_00E0();
 
-    // JMP address
-    void OP_1nnn();
+	// RET
+	void OP_00EE();
 
-    // Call address
-    void OP_2nnn();
+	// JP address
+	void OP_1nnn();
 
-    // SE Vx, byte
-    void OP_3xkk();
+	// CALL address
+	void OP_2nnn();
 
-    
+	// SE Vx, byte
+	void OP_3xkk();
 
+	// SNE Vx, byte
+	void OP_4xkk();
 
-    std::default_random_engine randGen;
-    std::uniform_int_distribution<uint8_t> randByte;
+	// SE Vx, Vy
+	void OP_5xy0();
+
+	// LD Vx, byte
+	void OP_6xkk();
+
+	// ADD Vx, byte
+	void OP_7xkk();
+
+	// LD Vx, Vy
+	void OP_8xy0();
+
+	// OR Vx, Vy
+	void OP_8xy1();
+
+	// AND Vx, Vy
+	void OP_8xy2();
+
+	// XOR Vx, Vy
+	void OP_8xy3();
+
+	// ADD Vx, Vy
+	void OP_8xy4();
+
+	// SUB Vx, Vy
+	void OP_8xy5();
+
+	// SHR Vx
+	void OP_8xy6();
+
+	// SUBN Vx, Vy
+	void OP_8xy7();
+
+	// SHL Vx
+	void OP_8xyE();
+
+	// SNE Vx, Vy
+	void OP_9xy0();
+
+	// LD I, address
+	void OP_Annn();
+
+	// JP V0, address
+	void OP_Bnnn();
+
+	// RND Vx, byte
+	void OP_Cxkk();
+
+	// DRW Vx, Vy, height
+	void OP_Dxyn();
+
+	// SKP Vx
+	void OP_Ex9E();
+
+	// SKNP Vx
+	void OP_ExA1();
+
+	// LD Vx, DT
+	void OP_Fx07();
+
+	// LD Vx, K
+	void OP_Fx0A();
+
+	// LD DT, Vx
+	void OP_Fx15();
+
+	// LD ST, Vx
+	void OP_Fx18();
+
+	// ADD I, Vx
+	void OP_Fx1E();
+
+	// LD F, Vx
+	void OP_Fx29();
+
+	// LD B, Vx
+	void OP_Fx33();
+
+	// LD [I], Vx
+	void OP_Fx55();
+
+	// LD Vx, [I]
+	void OP_Fx65();
+
+	uint8_t memory[MEMORY_SIZE]{};
+	uint8_t registers[REGISTER_COUNT]{};
+	uint16_t index{};
+	uint16_t pc{};
+	uint8_t delayTimer{};
+	uint8_t soundTimer{};
+	uint16_t stack[STACK_LEVELS]{};
+	uint8_t sp{};
+	uint16_t opcode{};
+
+	std::default_random_engine randGen;
+	std::uniform_int_distribution<uint8_t> randByte;
+
+	typedef void (Chip8::*Chip8Func)();
+	Chip8Func table[0xF + 1];
+	Chip8Func table0[0xE + 1];
+	Chip8Func table8[0xE + 1];
+	Chip8Func tableE[0xE + 1];
+	Chip8Func tableF[0x65 + 1];
 };
+
 
 # endif
